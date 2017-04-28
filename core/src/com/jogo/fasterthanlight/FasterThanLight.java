1eb65d7e5c7cf9946ed271f13f391b0b70006361
jviitsm <jvictorgmodesto@gmail.com>
2017-04-28 18:17:40 -0300

    Commit inicial

---
+++ core/src/com/jogo/fasterthanlight/FasterThanLight.java
@@ -0,0 +1,29 @@
+package com.jogo.fasterthanlight;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FasterThanLight extends ApplicationAdapter {
+
+    public Texture carro,fundo;
+	
+	@Override
+	public void create () {
+
+	carro = new Texture("carroPrincipal.png");
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
